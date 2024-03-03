6c1d1012b5e8e8a654ea6752ae80e11816ac9101
TheSimpleProgrammer9999 <162026962+TheSimpleProgrammer9999@users.noreply.github.com>
2024-03-03 14:43:31 -0500

    ProgramScripter.py

--- ProgramScripter.py
+++ ProgramScripter.py
@@ -1,6 +1,8 @@
 import time
-from error_syntax import error
 
+
+def text(text_):
+    print(text_)
 
 while True:
     inputter=input(">>")
@@ -19,70 +21,75 @@ while True:
         var_create2=inputv_
         _inputvalue=input("Store value in class <var> ")
         stuffstoreinvar2=_inputvalue
+        
+    elif inputter=="CreateVar(:3)":
+        inputv__=input("\t>>")
+        var_create3=inputv__
+        inputvalue__=input("Store value in class <var> ")
+        stuffstoreinvar3=inputvalue__
+        
+    elif inputter=="CreateVar(:4)":
+        inputv___=input("\t>>")
+        var_create4=inputv___
+        __inputvalue=input("Store value in class <var> ")
+        stuffstoreinvar4=__inputvalue
+
+    elif inputter=="credits(True)":
+        text("\t-----credits-----\nProgrammed by the GitHub user TheSimpleProgrammer9999\nThis is a simple programming language,\ncalled ProgramScripter.The programming language\nintroduces basic mechanics, such\nas simple arithmetic, and variables. But it does not support lists or syntax errors. It\nonly supports basic arithmetic, printing stuff on the screen,\nprinting variables. The owner who write this programming language is\nnot a Python developer. He doesn't know how to code a syntax\nor whatever that is complex. So it is a very simple programming language.\nIt also doesn't support files either. You write code in the terminal\nin this programming language.")
+
+    elif inputter=="<close>":
+        text("Interpreter:: <closing>")
+        break
     try:
-        if inputter==(f"write_var({var_create})"):
+        if inputter==(f"write_var() {var_create}"):
             print(stuffstoreinvar)
 
-        elif inputter==(f"write_var({var_create2})"):
+        elif inputter==(f"write_var() {var_create2}"):
             print(stuffstoreinvar2)
+            
+        elif inputter==(f"write_var() {var_create3}"):
+            print(stuffstoreinvar3)
+            
+        elif inputter==(f"write_var() {var_create4}"):
+            print(stuffstoreinvar4)
     except NameError:
         pass
 
-    if inputter=="calculate(+)":
-        cal=input("first integer: ")
-        firstint=cal
-        cal2=input("second integer: ")
-        secondint=cal2
-        print(int(firstint) + int(secondint))
-    
-    elif inputter=="calculate(-)":
-        cal=input("first integer: ")
-        firstint=cal
-        cal2=input("second integer: ")
-        secondint=cal2
-        print(int(firstint) - int(secondint))
-            
-    elif inputter=="calculate(*)":
-        cal=input("first integer: ")
-        firstint=cal
-        cal2=input("second integer: ")
-        secondint=cal2
-        print(int(firstint) * int(secondint))
-            
-    elif inputter=="calculate(/)":
-        cal=input("first integer: ")
-        firstint=cal
-        cal2=input("second integer: ")
-        secondint=cal2
-        print(int(firstint) / int(secondint))
     try:
-        if inputter=="string_calculate(+)":
-            cal=input("first string: ")
-            firststr=cal
-            cal2=input("second string: ")
-            secondstr=cal2
-            print(int(var_create) + int(var_create2))
-        
-        if inputter=="calculate(-)":
+        if inputter=="calculate(+)":
             cal=input("first integer: ")
-            firststr=cal
+            firstint=cal
             cal2=input("second integer: ")
-            secondstr=cal2
+            secondint=cal2
+            print(int(firstint) + int(secondint))
+        
+        elif inputter=="calculate(-)":
+            cal=input("first integer: ")
+            firstint=cal
+            cal2=input("second integer: ")
+            secondint=cal2
             print(int(firstint) - int(secondint))
                 
-        if inputter=="calculate(*)":
+        elif inputter=="calculate(*)":
             cal=input("first integer: ")
-            firststr=cal
+            firstint=cal
             cal2=input("second integer: ")
-            secondstr=cal2
+            secondint=cal2
             print(int(firstint) * int(secondint))
                 
-        if inputter=="calculate(/)":
+        elif inputter=="calculate(/)":
             cal=input("first integer: ")
-            firststr=cal
+            firstint=cal
             cal2=input("second integer: ")
-            secondstr=cal2
+            secondint=cal2
             print(int(firstint) / int(secondint))
-    except NameError:
-        error(f"interpreter_interrupted():\n\tVariable {firststr}, {secondstr} not availiable")
-    
+                            
+        elif inputter=="calculate(%)":
+            cal=input("first integer: ")
+            firstint=cal
+            cal2=input("second integer: ")
+            secondint=cal2
+            print(int(firstint) % int(secondint))
+
+    except ZeroDivisionError:
+       pass
