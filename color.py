4bd61ee0c26ccb0722afefb2ba9ece18c8d64ade
TheSimpleProgrammer9999 <162026962+TheSimpleProgrammer9999@users.noreply.github.com>
2024-03-05 14:26:07 -0500

    Add files via upload
    An esolang created by me
    try to figure it out to use it
    it only introduces lists, printing variables, printing lists, and printing
    
    look in my "color_uses.txt" file in the repository for more details

---
+++ color.py
@@ -0,0 +1,19 @@
+var=None
+List1=[]
+
+
+while True:
+    input_=input("purple  ")
+    if input_=="black green--green black":
+        printinput=input("-- ")
+        print(printinput)
+    elif input_=="black black -?":
+        varinput=input("-? ")
+        var=varinput
+    elif input_=="black black-black-black black":
+        print(var)
+    elif input_=="black--black black black -?- black":
+        listinput=input("-?- ")
+        item=listinput
+        List1.append(item)
+        print(List1)
